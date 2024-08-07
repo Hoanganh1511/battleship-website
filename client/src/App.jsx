02e8b11a7668b7df3dfdff186fbb241b7cd45e55
jiminsample1 <jiminsample1@gmail.com>
2024-08-08 02:53:49 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useEffect, useLayoutEffect } from "react";
 function App() {
   const userSquares = [];
   const computerSquares = [];
@@ -76,53 +73,114 @@ function App() {
     else generate(ship);
     console.log(123, ship);
   }
-  useEffect(() => {
+  useLayoutEffect(() => {
     const userGrid = document.querySelector(".grid-user");
     const computerGrid = document.querySelector(".grid-computer");
-    const createBoard = (grid, squares) => {
+    const createBoard = (grid, squares, type) => {
       for (let i = 0; i < width * width; i++) {
         const square = document.createElement("div");
-        square.classList.add("square");
+        type === "your" && square.classList.add("square", "your-square");
+        type === "opponent" && square.classList.add("square");
         square.dataset.id = i;
         grid.appendChild(square);
         squares.push(square);
       }
     };
     if (userGrid && userSquares && computerGrid && computerSquares) {
-      createBoard(userGrid, userSquares);
-      createBoard(computerGrid, computerSquares);
+      createBoard(userGrid, userSquares, "your");
+      createBoard(computerGrid, computerSquares, "opponent");
     }
     generate(shipArray[0]);
+    generate(shipArray[1]);
+    generate(shipArray[2]);
+    generate(shipArray[3]);
+    generate(shipArray[4]);
   }, []);
   // Create board
 
   return (
     <>
       <h1 className="name-game">Battle ship</h1>
-      <div className="header">
-        <div className="player p1">
-          <label>Player1</label>
-          <p>Tuananh</p>
-          <div className="connected">
-            <span className="badge"></span>
-            <span>Connected</span>
-          </div>
-          <div className="ready">Ready</div>
-        </div>
-        <div className="player p2">
-          <label>Player2</label>
-          <p>Tuananh</p>
-          <div className="connected">
-            <span>Connected</span>
-            <span className="badge"></span>
-          </div>
-          <div className="ready">Ready</div>
-        </div>
-      </div>
-
       <div className="container">
-        <div className="battleship-grid grid-user"></div>
-        <div className="battleship-grid grid-computer"></div>
+        <div className="header">
+          <div className="player p1">
+            <label>Player1</label>
+            <div className="connected">
+              <span className="badge"></span>
+              <span>Connected</span>
+            </div>
+            <div className="ready">Ready</div>
+          </div>
+          <div className="player p2">
+            <label>Player2</label>
+            <div className="connected">
+              <span>Connected</span>
+              <span className="badge"></span>
+            </div>
+            <div className="ready">Ready</div>
+          </div>
+        </div>
+
+        <div className="battleship-container">
+          <div className="battleship-area__area your-turn">
+            <div className="name">Your Turn</div>
+            <div className="battleship-grid grid-user">
+              <div className="row">
+                <span>A</span>
+                <span>B</span>
+                <span>C</span>
+                <span>D</span>
+                <span>E</span>
+                <span>F</span>
+                <span>G</span>
+                <span>H</span>
+                <span>I</span>
+                <span>J</span>
+              </div>
+              <div className="col">
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+                <span>4</span>
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span>8</span>
+                <span>9</span>
+                <span>10</span>
+              </div>
+            </div>
+          </div>
+          <div className="battleship-area__area opponent-turn">
+            <div className="name">Opponent Turn</div>
+            <div className="battleship-grid grid-computer">
+              <div className="row">
+                <span>A</span>
+                <span>B</span>
+                <span>C</span>
+                <span>D</span>
+                <span>E</span>
+                <span>F</span>
+                <span>G</span>
+                <span>H</span>
+                <span>I</span>
+                <span>J</span>
+              </div>
+              <div className="col">
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+                <span>4</span>
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span>8</span>
+                <span>9</span>
+                <span>10</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
